0bf7afa20f4e072057cbe8cf93bd3da960535561
Murad Huseynli <162417735+muradcodeacademy@users.noreply.github.com>
2024-05-02 08:19:58 +0400

    Update Cards add sort

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,65 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import controller from "../../services";
-import { endpoints } from "../../services/constants";
-import Container from 'react-bootstrap/Container';
-import CardItem from '../CardItem';
-import Row from 'react-bootstrap/Row';
-import SearchInp from '../SearchInp';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import { useState, useEffect } from 'react'
+import controller from '../../services'
+import { endpoints } from '../../services/constants'
+import Container from 'react-bootstrap/Container'
+import CardItem from '../CardItem'
+import Row from 'react-bootstrap/Row'
+import SearchInp from '../SearchInp'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 
 function Cards() {
-    const [artists, setArtists] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [query, setQuery] = useState("");
+	const [artists, setArtists] = useState([])
+	const [filteredArtists, setFilteredArtists] = useState([])
+	const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        controller
-            .getAll(endpoints.artists)
-            .then((res) => {
-                setArtists(res.data);
-            })
-            .finally(() => {
-                setLoading(false);
-            })
-    }, []);
-    const filteredArtists = artists.filter((artist) => {
-        return artist.name.trim().toLowerCase().startsWith(query.trim().toLowerCase());
-    });
+	useEffect(() => {
+		controller
+			.getAll(endpoints.artists)
+			.then((res) => {
+				setArtists(res.data)
+				setFilteredArtists(res.data)
+			})
+			.finally(() => {
+				setLoading(false)
+			})
+	}, [])
 
-    
-    return (
-        <Container className='mt-5'>
-            <Row className='d-flex justify-content-end' >
-                <div className='mb-5 w-25'>
-                    <SearchInp setQuery={setQuery} />
-                </div>
-                <FormControl className='w-25' sx={{minWidth: 120 }} size="small">
-                    <InputLabel id="demo-simple-select-label">Sort</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        label="Age"
-                    >
-                        <MenuItem value={10}>A-Z</MenuItem>
-                        <MenuItem value={20}>Z-A</MenuItem>
-                    </Select>
-                </FormControl>
-            </Row>
-            <Row>
-                {
-                    filteredArtists &&
-                    filteredArtists.map((artist) =>
-                        <CardItem key={artist.id} artist={artist} loading={loading} setArtists={setArtists} />
-                    )
+	const search = (query) => {
+		setFilteredArtists(
+			artists.filter((artist) => {
+				return artist.name
+					.trim()
+					.toLowerCase()
+					.startsWith(query.trim().toLowerCase())
+			})
+		)
+	}
 
-                }
+	const sortedArtists = (direction) => {
+		let sorted = [...filteredArtists]
 
-            </Row>
-        </Container>
-    );
+		sorted.sort((a, b) => {
+			if (direction === 'asc') {
+				return a.name.localeCompare(b.name)
+			} else if (direction === 'desc') {
+				return b.name.localeCompare(a.name)
+			}
+		})
+
+		setFilteredArtists(sorted)
+	}
+
+	return (
+		<Container className='mt-5'>
+			<Row className='d-flex justify-content-end'>
+				<div className='mb-5 w-25'>
+					<SearchInp search={search} />
+				</div>
+				<FormControl
+					className='w-25'
+					sx={{ minWidth: 120 }}
+					size='small'
+				>
+					<InputLabel id='demo-simple-select-label'>Sort</InputLabel>
+					<Select onChange={(e) => sortedArtists(e.target.value)}>
+						<MenuItem value='asc'>A-Z</MenuItem>
+						<MenuItem value='desc'>Z-A</MenuItem>
+					</Select>
+				</FormControl>
+			</Row>
+			<Row>
+				{filteredArtists &&
+					filteredArtists.map((artist) => (
+						<CardItem
+							key={artist.id}
+							artist={artist}
+							loading={loading}
+							setArtists={setArtists}
+						/>
+					))}
+			</Row>
+		</Container>
+	)
 }
 
 export default Cards
